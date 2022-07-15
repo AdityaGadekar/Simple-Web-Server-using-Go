28b53200c5b2fc588d8874e20781186f4929119a
AdityaGadekar <aadityagadekar@gmail.com>
2022-07-15 13:40:13 +0530

    Web server using Go

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+ 
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+ 
+func main() {
+ 
+  
+    http.Handle("/", http.FileServer(http.Dir("./Form")))
+ 
+    // Serve api /hi
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hi")
+    })
+ 
+    port := ":5000"
+    fmt.Println("Server is running on port" + port)
+ 
+    // Start server on port specified above
+    log.Fatal(http.ListenAndServe(port, nil))
+ 
+}
